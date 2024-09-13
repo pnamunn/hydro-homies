fbaeca13f350aa9fb31f3ff402551096edb4ec41
pnamunn <pnamunn@gmail.com>
2024-09-14 00:04:20 +0200

    working GPIO led

--- main/station_example_main.c
+++ main/station_example_main.c
@@ -20,18 +20,19 @@
 #include <time.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <driver/gpio.h>
 
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
-/* The examples use WiFi configuration that you can set via project configuration menu
-
-   If you'd rather not, just change the below entries to strings with
-   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
-*/
-#define EXAMPLE_ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
-#define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
+// Wifi menuconfigs
+#define EXAMPLE_ESP_WIFI_SSID      "DALLEVIGNE-SKY"
+#define EXAMPLE_ESP_WIFI_PASS      "welcome2dallevigne"
 #define EXAMPLE_ESP_MAXIMUM_RETRY  4
+
+// GPIO menuconfigs
+#define LED_GPIO 5
+
 
 #if CONFIG_ESP_WPA3_SAE_PWE_HUNT_AND_PECK
 #define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_HUNT_AND_PECK
@@ -70,21 +71,20 @@ static EventGroupHandle_t s_wifi_event_group;
 #define WIFI_CONNECTED_BIT BIT0
 #define WIFI_FAIL_BIT      BIT1
 
-static const char *STA = "station wifi";
-static const char *NTP = "network time protocol";
+static const char* STA = "STA";
+static const char* NTP = "NTP";
+static const char* GPIO = "GPIO";
 static int retry_count = 0;
 
-// const char* NTPServer = "pool.ntp.org";
-// const long gmtOffset_sec = -7 * 60 * 60;   // PST timezone
-// const int daylightOffset_sec = 1 * 60 * 60;    // is daylight savings
 
+static void wifi_connection_events_handler
+            (void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
 
-static void event_handler(void* arg, esp_event_base_t event_base,
-                                int32_t event_id, void* event_data)
-{
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+    // if wifi has been started in station mode
         esp_wifi_connect();
     } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+    // if wifi fails to connect or is called to disconnect
         if (retry_count < EXAMPLE_ESP_MAXIMUM_RETRY) {
             esp_wifi_connect();
             retry_count++;
@@ -94,6 +94,7 @@ static void event_handler(void* arg, esp_event_base_t event_base,
         }
         ESP_LOGI(STA,"Connect attempt to the AP failed");
     } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+    // if wifi successfully got IPv4 from DHCP (ready to begin tasks)
         ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
         ESP_LOGI(STA, "Got ip:" IPSTR, IP2STR(&event->ip_info.ip));
         retry_count = 0;
@@ -102,8 +103,7 @@ static void event_handler(void* arg, esp_event_base_t event_base,
 }
 
 
-void wifi_init_sta(void)
-{
+void wifi_init_sta(void) {
     s_wifi_event_group = xEventGroupCreate();
 
     // use NETIF lib for application layer abstraction & thread safety
@@ -114,7 +114,6 @@ void wifi_init_sta(void)
     esp_sntp_config_t sntp_configs = ESP_NETIF_SNTP_DEFAULT_CONFIG(ntp_server_address);
     ESP_ERROR_CHECK(esp_netif_sntp_init(&sntp_configs));
     ESP_LOGI(NTP, "Service started from %s", ntp_server_address);
-
 
     // set PST timezone
     char timezone[] = "PST8PDT";
@@ -130,19 +129,20 @@ void wifi_init_sta(void)
     wifi_init_config_t wifi_configs = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&wifi_configs));
 
+    // handler events
     esp_event_handler_instance_t instance_any_id;
     esp_event_handler_instance_t instance_got_ip;
 
-    // check if got ID
+    // handler check if got wifi ID data
     ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
                                                         ESP_EVENT_ANY_ID,
-                                                        &event_handler,
+                                                        &wifi_connection_events_handler,
                                                         NULL,
                                                         &instance_any_id));
-    // check if got IP
+    // handler check if got IP data
     ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
                                                         IP_EVENT_STA_GOT_IP,
-                                                        &event_handler,
+                                                        &wifi_connection_events_handler,
                                                         NULL,
                                                         &instance_got_ip));
 
@@ -163,19 +163,19 @@ void wifi_init_sta(void)
 
     // set sta mode, configs, & start wifi
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &sta_configs) );
-    ESP_ERROR_CHECK(esp_wifi_start() );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &sta_configs));
+    ESP_ERROR_CHECK(esp_wifi_start());
 
     /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
-     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by wifi_event_handler() (see above) */
     EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
             WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
             pdFALSE,
             pdFALSE,
             portMAX_DELAY);
 
-    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
-     * happened. */
+    /* xEventGroupWaitBits() returns the bits before the call returned,
+    hence we can test which event actually happened. */
     if (bits & WIFI_CONNECTED_BIT) {
         ESP_LOGI(STA, "Successfully connected to AP SSID: %s", EXAMPLE_ESP_WIFI_SSID);
     } else if (bits & WIFI_FAIL_BIT) {
@@ -200,13 +200,30 @@ void app_main(void)
 
     ESP_LOGI(STA, "ESP_WIFI_MODE_STA");
     wifi_init_sta();
+    time_t seconds_since_epoch;
+    struct tm* current_time;
 
-    time_t current_time;
+    gpio_reset_pin(LED_GPIO);   // enables pullup
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+    ESP_LOGI(GPIO, "Init GPIO pin 5 to output");
+
 
     while(1) {
-        sleep(10);
+        // sleep(10);
+        uint16_t delay_seconds = 10;
+        vTaskDelay((delay_seconds * 1000) / portTICK_PERIOD_MS);
 
-        current_time = time(NULL);
-        printf("Current time is:  %s", asctime(localtime(&current_time)));        
+        seconds_since_epoch = time(NULL);
+        current_time = localtime(&seconds_since_epoch);
+        printf("Current time is:  %s", asctime(current_time));
+
+
+        // if the second's tens place is odd, turn led on; if even, turn led off
+        uint32_t led_level = (current_time->tm_sec / 10) % 2;
+        gpio_set_level(LED_GPIO, led_level);
+        ESP_LOGI(GPIO, "Turning led to %"PRIu32"", led_level);
+
+
     }
 }
+
